ea23cb9550178677a2179ed63203313dbf6c577f
ddatsko <ddatsko010101@gmail.com>
2020-12-16 12:02:32 +0200

    Fixed bugs in d7seg.c

--- d7seg.c
+++ d7seg.c
@@ -6,6 +6,7 @@
 #include <linux/gpio.h>
 
 extern int read_digit_from_7seg(void);
+
 extern int write_digit_to_7seg(int digit);
 
 
@@ -15,6 +16,10 @@ extern int write_digit_to_7seg(int digit);
 
 int printed_digit = -1;
 
+
+enum state {
+    low, high
+};
 
 
 const unsigned char mask[10][7] = {
@@ -32,7 +37,6 @@ const unsigned char mask[10][7] = {
 };
 
 const unsigned int symbols[7] = {5, 17, 22, 23, 24, 25, 27};
-
 
 
 struct sevenseg_lkm_dev {
@@ -74,18 +78,15 @@ static dev_t first;
 static struct class *sevenseg_lkm_class;
 
 
-
-
 static int sevenseg_lkm_open(struct inode *inode, struct file *filp) {
     struct sevenseg_lkm_dev *sevenseg_lkm_devp;
-    unsigned int gpio;
 
 
     printk(KERN_INFO
-    "[7SEG-LKM] -  opened\n");
+           "[7SEG-LKM] -  opened\n");
 
     sevenseg_lkm_devp = container_of(inode->i_cdev,
-    struct sevenseg_lkm_dev, cdev);
+                                     struct sevenseg_lkm_dev, cdev);
 
     filp->private_data = sevenseg_lkm_devp;
 
@@ -95,31 +96,26 @@ static int sevenseg_lkm_open(struct inode *inode, struct file *filp) {
 
 
 static int sevenseg_lkm_release(struct inode *inode, struct file *filp) {
-    unsigned int gpio;
 
 
     filp->private_data = NULL;
     /* print debug message to system journal - good practice */
     printk(KERN_INFO
-    "[GPIO-LKM] - Closing\n");
+           "[GPIO-LKM] - Closing\n");
 
     return 0;
 }
-
-
 
 
 extern int read_digit_from_7seg(void) {
     return printed_digit;
 }
 
-EXPORT_SYMBOL ( read_digit_from_7seg );
+EXPORT_SYMBOL (read_digit_from_7seg);
 
 static ssize_t sevenseg_lkm_read(struct file *filp, char *buf, size_t count, loff_t *f_pos) {
-    unsigned int gpio;
     ssize_t retval;
     char byte;
-    struct sevenseg_lkm_dev *sevenseg_lkm_devp = filp->private_data;
 
 
     for (retval = 0; retval < count; ++retval) {
@@ -158,16 +154,13 @@ extern int write_digit_to_7seg(int digit) {
     }
     return 0;
 }
-EXPORT_SYMBOL( write_digit_to_7seg );
 
-
+EXPORT_SYMBOL(write_digit_to_7seg);
 
 
 static ssize_t sevenseg_lkm_write(struct file *filp, const char *buf, size_t count, loff_t *f_pos) {
-    unsigned int gpio, len = 0, i;
+    unsigned int len = 0, i;
     char kbuf[BUF_SIZE];
-    struct sevenseg_lkm_dev *sevenseg_lkm_devp = filp->private_data;
-
 
     len = count < BUF_SIZE ? count - 1 : BUF_SIZE - 1;
 
@@ -181,7 +174,7 @@ static ssize_t sevenseg_lkm_write(struct file *filp, const char *buf, size_t cou
         printed_digit = -1;
 
         printk(KERN_INFO
-        "[7SEG-LKM] - cannot set 7seg to %s\n", kbuf);
+               "[7SEG-LKM] - cannot set 7seg to %s\n", kbuf);
         return -1;
     }
     if (write_digit_to_7seg(buf[0] - '0')) {
@@ -189,7 +182,7 @@ static ssize_t sevenseg_lkm_write(struct file *filp, const char *buf, size_t cou
         return -1;
     }
 
-    printed_digit =  buf[0] - '0';
+    printed_digit = buf[0] - '0';
 
     *f_pos += count;
     return count;
@@ -203,14 +196,14 @@ sevenseg_lkm_init(void) {
 
     if (alloc_chrdev_region(&first, 0, 1, DEVICE_NAME) < 0) {
         printk(KERN_DEBUG
-        "Cannot register device\n");
+               "Cannot register device\n");
         return -1;
     }
 
 
     if ((sevenseg_lkm_class = class_create(THIS_MODULE, DEVICE_NAME)) == NULL) {
         printk(KERN_DEBUG
-        "Cannot create class %s\n", DEVICE_NAME);
+               "Cannot create class %s\n", DEVICE_NAME);
 
         unregister_chrdev_region(first, 1);
 
@@ -219,17 +212,16 @@ sevenseg_lkm_init(void) {
 
     sevenseg_lkm_devp = kmalloc(sizeof(struct sevenseg_lkm_dev), GFP_KERNEL);
 
-    if (!sevenseg_lkm_devp)
-    {
+    if (!sevenseg_lkm_devp) {
         printk(KERN_DEBUG
-        "[7SEG-LKM]: Bad kmalloc\n");
+               "[7SEG-LKM]: Bad kmalloc\n");
         return -ENOMEM;
     }
 
     for (index = 0; index < 7; ++index)
         if (gpio_request_one(symbols[index], GPIOF_OUT_INIT_LOW, NULL) < 0) {
             printk(KERN_ALERT
-            "[7SEG-LKM] - Error requesting GPIO %d\n", i);
+                   "[7SEG-LKM] - Error requesting GPIO %d\n", i);
             return -ENODEV;
         }
 
@@ -239,7 +231,7 @@ sevenseg_lkm_init(void) {
 
     if ((ret = cdev_add(&sevenseg_lkm_devp->cdev, first, 1))) {
         printk(KERN_ALERT
-        "[7SEG-LKM] - Error %d adding cdev\n", ret);
+               "[7SEG-LKM] - Error %d adding cdev\n", ret);
 
 
         device_destroy(sevenseg_lkm_class,
@@ -285,7 +277,7 @@ sevenseg_lkm_exit(void) {
      */
     class_destroy(sevenseg_lkm_class);
     printk(KERN_INFO
-    "[7SEG-LKM] - Raspberry Pi GPIO driver removed\n");
+           "[7SEG-LKM] - Raspberry Pi GPIO driver removed\n");
 }
 
 /* these are stantard macros to mark
@@ -295,5 +287,7 @@ module_init(sevenseg_lkm_init);
 module_exit(sevenseg_lkm_exit);
 
 MODULE_LICENSE("GPL");
+
 MODULE_AUTHOR("Denys Datsko, Nazar Pasternak, Sofia Petryshyn");
+
 MODULE_DESCRIPTION("7seg Loadable Kernel Module - Linux device driver for Raspberry Pi for 7 segment display");
